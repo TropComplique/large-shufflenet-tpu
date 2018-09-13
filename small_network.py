65d06d9c11e6b718b1faabc38d17b54126889e75
TropComplique <antoshchenko.b@gmail.com>
2018-09-13 13:54:28 +0300

    initial

--- small_network.py
+++ small_network.py
@@ -1,10 +1,5 @@
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
-from utils import block
-
-
-BATCH_NORM_MOMENTUM = 0.9
-BATCH_NORM_EPSILON = 1e-3
+from utils import block, batch_norm_relu
 
 
 def shufflenet(images, is_training, num_classes=1000, depth_multiplier='1.0'):
@@ -25,42 +20,27 @@ def shufflenet(images, is_training, num_classes=1000, depth_multiplier='1.0'):
     # 116 for '1.0' in the original paper
     initial_depth = possibilities[depth_multiplier]
 
-    def batch_norm(x):
-        x = tf.layers.batch_normalization(
-            x, axis=3, center=True, scale=True,
-            training=is_training,
-            momentum=BATCH_NORM_MOMENTUM,
-            epsilon=BATCH_NORM_EPSILON,
-            fused=True, name='batch_norm'
-        )
-        return x
-
     with tf.name_scope('standardize_input'):
         x = (2.0 * images) - 1.0
 
     with tf.variable_scope('ShuffleNetV2'):
-        params = {
-            'padding': 'SAME', 'activation_fn': tf.nn.relu,
-            'normalizer_fn': batch_norm, 'data_format': 'NHWC',
-            'weights_initializer': tf.contrib.layers.xavier_initializer()
-        }
-        with slim.arg_scope([slim.conv2d, slim.separable_conv2d], **params):
 
-            x = slim.conv2d(x, 24, (3, 3), stride=2, scope='Conv1')
-            x = slim.max_pool2d(x, (3, 3), stride=2, padding='SAME', scope='MaxPool')
+        x = tf.layers.conv2d(x, 24, (3, 3), strides=(2, 2), use_bias=False, padding='same', name='Conv1')
+        x = batch_norm_relu(x, is_training, name='bn')
+        x = tf.layers.max_pooling2d(x, (3, 3), strides=(2, 2), padding='same', name='MaxPool')
 
-            x = block(x, num_units=4, out_channels=initial_depth, scope='Stage2')
-            x = block(x, num_units=8, scope='Stage3')
-            x = block(x, num_units=4, scope='Stage4')
+        x = block(x, is_training, num_units=4, out_channels=initial_depth, scope='Stage2')
+        x = block(x, is_training, num_units=8, scope='Stage3')
+        x = block(x, is_training, num_units=4, scope='Stage4')
 
-            final_channels = 1024 if depth_multiplier != '2.0' else 2048
-            x = slim.conv2d(x, final_channels, (1, 1), stride=1, scope='Conv5')
+        final_channels = 1024 if depth_multiplier != '2.0' else 2048
+        x = tf.layers.conv2d(x, final_channels, (1, 1), strides=(1, 1), use_bias=False, padding='same', name='Conv5')
 
     # global average pooling
     x = tf.reduce_mean(x, axis=[1, 2])
 
-    logits = slim.fully_connected(
-        x, num_classes, activation_fn=None, scope='classifier',
-        weights_initializer=tf.contrib.layers.xavier_initializer()
+    logits = tf.layers.dense(
+        x, num_classes, name='classifier',
+        kernel_initializer=tf.contrib.layers.xavier_initializer()
     )
     return logits
--- utils.py
+++ utils.py
@@ -1,20 +1,19 @@
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
 
 
-def block(x, num_units, out_channels=None, downsample=True, scope='stage'):
+def block(x, is_training, num_units, out_channels=None, downsample=True, scope='stage'):
     with tf.variable_scope(scope):
 
         with tf.variable_scope('unit_1'):
             x, y = basic_unit_with_downsampling(
-                x, out_channels=out_channels,
+                x, is_training, out_channels=out_channels,
                 downsample=downsample
             )
 
         for j in range(2, num_units + 1):
             with tf.variable_scope('unit_%d' % j):
                 x, y = concat_shuffle_split(x, y)
-                x = basic_unit(x)
+                x = basic_unit(x, is_training)
         x = tf.concat([x, y], axis=3)
 
     return x
@@ -40,24 +39,62 @@ def concat_shuffle_split(x, y):
         return x, y
 
 
-def basic_unit(x):
+def basic_unit(x, is_training, is_training):
     in_channels = x.shape[3].value
-    x = slim.conv2d(x, in_channels, (1, 1), stride=1, scope='conv1x1_before')
-    x = slim.separable_conv2d(x, None, (3, 3), stride=1, depth_multiplier=1, activation_fn=None, scope='depthwise')
-    x = slim.conv2d(x, in_channels, (1, 1), stride=1, scope='conv1x1_after')
+    x = tf.layers.conv2d(x, in_channels, (1, 1), strides=(1, 1), use_bias=False, padding='same', name='conv1x1_before')
+    x = batch_norm_relu(x, is_training, name='bn1')
+    x = depthwise_conv(x, stride=1, name='depthwise')
+    x = batch_norm_relu(x, is_training, use_relu=False, name='bn2')
+    x = tf.layers.conv2d(x, in_channels, (1, 1), strides=(1, 1), use_bias=False, padding='same', name='conv1x1_after')
+    x = batch_norm_relu(x, is_training, , name='bn3')
     return x
 
 
-def basic_unit_with_downsampling(x, out_channels=None, downsample=True):
+def basic_unit_with_downsampling(x, is_training, out_channels=None, downsample=True):
     in_channels = x.shape[3].value
     out_channels = 2 * in_channels if out_channels is None else out_channels
     stride = 2 if downsample else 1  # paradoxically, it sometimes doesn't downsample
 
-    y = slim.conv2d(x, in_channels, (1, 1), stride=1, scope='conv1x1_before')
-    y = slim.separable_conv2d(y, None, (3, 3), stride=stride, depth_multiplier=1, activation_fn=None, scope='depthwise')
-    y = slim.conv2d(y, out_channels // 2, (1, 1), stride=1, scope='conv1x1_after')
+    x = tf.layers.conv2d(x, in_channels, (1, 1), strides=(1, 1), use_bias=False, padding='same', name='conv1x1_before')
+    y = batch_norm_relu(y, is_training, name='bn1')
+    y = depthwise_conv(y, stride=stride, name='depthwise')
+    y = batch_norm_relu(y, is_training, use_relu=False, name='bn2')
+    y = tf.layers.conv2d(y, out_channels // 2, (1, 1), strides=(1, 1), use_bias=False, padding='same', name='conv1x1_after')
+    y = batch_norm_relu(y, is_training, , name='bn3')
 
     with tf.variable_scope('second_branch'):
-        x = slim.separable_conv2d(x, None, (3, 3), stride=stride, depth_multiplier=1, activation_fn=None, scope='depthwise')
-        x = slim.conv2d(x, out_channels // 2, (1, 1), stride=1, scope='conv1x1_after')
+        x = depthwise_conv(x, stride=stride, name='depthwise')
+        x = batch_norm_relu(x, is_training, use_relu=False, name='bn1')
+        x = tf.layers.conv2d(x, out_channels // 2, (1, 1), strides=(1, 1), use_bias=False, padding='same', name='conv1x1_after')
+        x = batch_norm_relu(x, is_training, , name='bn2')
         return x, y
+
+
+def depthwise_conv(x, stride, name):
+    with tf.variable_scope(name):
+        in_channels = x.shape[3].value
+        W = tf.get_variable(
+            'depthwise_weights',
+            [kernel, kernel, in_channels, 1],
+            initializer=tf.contrib.layers.xavier_initializer()
+        )
+        x = tf.nn.depthwise_conv2d(x, W, [1, stride, stride, 1], 'SAME', data_format='NHWC')
+        return x
+
+
+BATCH_NORM_MOMENTUM = 0.9
+BATCH_NORM_EPSILON = 1e-3
+
+
+def batch_norm_relu(x, is_training, use_relu=True, name='batch_norm'):
+    x = tf.layers.batch_normalization(
+        x, axis=3,
+        momentum=BATCH_NORM_MOMENTUM,
+        epsilon=BATCH_NORM_EPSILON,
+        center=True, scale=True,
+        training=is_training,
+        fused=True, name=name
+    )
+    if use_relu:
+        x = tf.nn.relu(x)
+    return x
